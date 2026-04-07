848c97778e0d20ae1f61bd1493e2d491758c5283
route60 roamer <markkengiealdabon@gmail.com>
2026-04-07 15:19:00 +0800

    Delete unnecessary import

--- app/api/veterinarian/sms/route.ts
+++ app/api/veterinarian/sms/route.ts
@@ -1,4 +1,3 @@
-import { createClient } from "@supabase/supabase-js";
 import { NextRequest, NextResponse } from "next/server";
 import { sendSms } from "@/utils/sms";
 import { createAdminClient } from "@/utils/supabase/server";
